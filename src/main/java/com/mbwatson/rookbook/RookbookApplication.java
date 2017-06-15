ac2e549d041ebab215c1d78081735032bc51c632
Matthew Watson <matthew.watson@interapthq.com>
2017-06-14 21:05:38 -0400

    Initial commit

---
+++ src/main/java/com/mbwatson/rookbook/RookbookApplication.java
@@ -0,0 +1,12 @@
+package com.mbwatson.rookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookbookApplication.class, args);
+	}
+}
